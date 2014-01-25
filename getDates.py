ff3376a8a5a204e1f84985f148a5dccdfe1c2afa
Goku <victor19@gmail.com>
2014-01-25 03:17:57 -0200

    denovo

--- getDates.py
+++ getDates.py
@@ -1,14 +1,22 @@
 import re
 import time
+from enviar import enviarEmail
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
-soup = BeautifulSoup(urlopen('http://community.topcoder.com/tc'))
-prizes = soup.find_all(attrs = {'class':'prizes'})
+while (True):
+	soup = BeautifulSoup(urlopen('http://community.topcoder.com/tc'))
+	prizes = soup.find_all(attrs = {'class':'prizes'})
 
-unformDate = prizes[-1]
-dates = unformDate.string.split()
+	unformDate = prizes[-1]
+	unformDate.string = unformDate.string.replace(',', '')
+	dates = unformDate.string.split()
 
-stringCurrentDates = time.strftime('%b %d')
-currentDates = stringCurrentDates.split()
+	stringCurrentDates = time.strftime('%b %d')
+	currentDates = stringCurrentDates.split()
 
-print(currentDates)
+	enviarEmail('teste', True)
+	if(int(currentDates[1]) + 1 == int(dates[1])):
+		enviarEmail('teste', True)
+	elif(int(currentDates[1]) == int(dates[1])):
+		enviarEmail('teste', False)
+	time.sleep(86400)

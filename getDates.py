034b7101c9b5e2539365acbdf10568b5a862563f
Goku <victor19@gmail.com>
2014-01-25 04:11:39 -0200

    merging

--- getDates.py
+++ getDates.py
@@ -3,18 +3,17 @@ import time
 from enviar import enviarEmail
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
-while (True):
-	soup = BeautifulSoup(urlopen('http://community.topcoder.com/tc'))
-	prizes = soup.find_all(attrs = {'class':'prizes'})
+soup = BeautifulSoup(urlopen('http://community.topcoder.com/tc'))
+prizes = soup.find_all(attrs = {'class':'prizes'})
 
-	unformDate = prizes[-1]
-	unformDate.string = unformDate.string.replace(',', '')
-	dates = unformDate.string.split()
+unformDate = prizes[-1]
+unformDate.string = unformDate.string.replace(',', '')
+dates = unformDate.string.split()
 
-	stringCurrentDates = time.strftime('%b %d')
-	currentDates = stringCurrentDates.split()
+stringCurrentDates = time.strftime('%b %d')
+currentDates = stringCurrentDates.split()
 
-	if(int(currentDates[1]) + 1 == int(dates[1])):
-		enviarEmail('', True)
-	elif(int(currentDates[1]) == int(dates[1])):
-		enviarEmail('', False)
+if(int(currentDates[1]) + 1 == int(dates[1])):
+	enviarEmail('', True)
+elif(int(currentDates[1]) == int(dates[1])):
+	enviarEmail('', False)

a91747e2d152b84160f011fbd260ebf90a326ad9
Goku <victor19@gmail.com>
2014-01-25 01:43:08 -0200

    separei a data numa lista

--- getDates.py
+++ getDates.py
@@ -1,5 +1,14 @@
+import re
+import time
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
-soup = BeautifulSoup(urlopen('http://community.topcoder.com/tc?module=MatchDetails&rd=15839'))
+soup = BeautifulSoup(urlopen('http://community.topcoder.com/tc'))
+prizes = soup.find_all(attrs = {'class':'prizes'})
 
-print(soup.prettify())
+unformDate = prizes[-1]
+dates = unformDate.string.split()
+
+stringCurrentDates = time.strftime('%b %d')
+currentDates = stringCurrentDates.split()
+
+print(currentDates)
